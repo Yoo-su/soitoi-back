6f88c20ad900751e3ef99f641dcc297b999c028e
yoosu <rhan0871@naver.com>
2025-02-14 14:55:59 +0900

    fix: chat socket message

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -3,6 +3,7 @@ import { Server, Socket } from 'socket.io';
 import { ChatService } from './chat.service';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { RandomUser } from 'src/shared/types';
+import { CHAT_CREATED, CHAT_UPDATES, TYPING_CHAT, TYPING_USERS } from './constants';
 
 @WebSocketGateway({ namespace: 'socket/chat', cors: { origin: '*' } })
 export class ChatGateway {
@@ -13,12 +14,12 @@ export class ChatGateway {
 
   private typingUsers = new Map<string, RandomUser>();
 
-  @SubscribeMessage('chat-created')
+  @SubscribeMessage(CHAT_CREATED)
   async create(@MessageBody() createChatDto: CreateChatDto, @ConnectedSocket() client: Socket) {
     const addedChat = await this.chatService.createChat(createChatDto);
     if (addedChat) {
       this.typingUsers.delete(client.id);
-      this.server.emit('chat-updates', {
+      this.server.emit(CHAT_UPDATES, {
         typingUsers: Array.from(this.typingUsers.values()),
         newChat: addedChat,
       });
@@ -40,7 +41,7 @@ export class ChatGateway {
     return this.chatService.remove(id);
   }
 
-  @SubscribeMessage('typing-chat')
+  @SubscribeMessage(TYPING_CHAT)
   handleTypingUser(@MessageBody() data: { user: RandomUser; isTyping: boolean }, @ConnectedSocket() client: Socket) {
     const { user, isTyping } = data;
     if (isTyping) {
@@ -48,14 +49,14 @@ export class ChatGateway {
     } else {
       this.typingUsers.delete(client.id);
     }
-    client.broadcast.emit('typing-users', Array.from(this.typingUsers.values()));
+    client.broadcast.emit(TYPING_USERS, Array.from(this.typingUsers.values()));
   }
 
   // ✅ 사용자가 연결되면 실행
   handleConnection(client: Socket) {
     const { user } = client.handshake.auth;
     console.log('new chat socket user:', user, 'clientID:', client.id);
-    client.emit('typing-users', Array.from(this.typingUsers.values()));
+    client.emit(TYPING_USERS, Array.from(this.typingUsers.values()));
   }
 
   // ✅ 사용자가 페이지를 닫거나 네트워크가 끊어지면 자동으로 실행
@@ -63,7 +64,7 @@ export class ChatGateway {
     if (this.typingUsers.has(client.id)) {
       this.typingUsers.delete(client.id);
       // 모든 클라이언트에 업데이트된 typingUsers 목록 전송
-      this.server.emit('typing-users', Array.from(this.typingUsers.values()));
+      this.server.emit(TYPING_USERS, Array.from(this.typingUsers.values()));
     }
   }
 }
